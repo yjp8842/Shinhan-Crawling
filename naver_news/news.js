41fc2aa757e941e16cc74c2153b923ef0129b2d7
yjp8842 <yjp20183029@gmail.com>
2024-01-30 11:39:19 +0900

    feat: 이미지 파일 저장 코드 작성 및 json 키 값을 영어로 수정

--- naver_news/news.js
+++ naver_news/news.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 import * as cheerio from 'cheerio';
+import https from 'https';
+import crypto from 'crypto';
 
 import fs from 'fs';
+
+fs.readdir('img', (err) => {
+  if (err) {
+    console.log('이미지 폴더가 없어서 생성합니다.');
+    fs.mkdirSync('img');
+  }
+})
 
 async function getData() {
   const url = 'https://search.naver.com/search.naver?where=news&sm=tab_jum&query=%EC%9D%B4%EC%B0%A8%EC%A0%84%EC%A7%80';
@@ -18,20 +27,35 @@ async function getData() {
       const summary = $(el).find('a.dsc_txt_wrap').text();
       const imgData = $(el).find('.dsc_thumb img').prop('data-lazysrc');
 
+      // 이미지 url을 이용해 이미지 파일로 저장
+      if (imgData) {
+        const imgResult = await axios.get(imgData, {
+          headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+          },
+          responseType: 'arraybuffer'
+        });
+
+        // console.log(imgResult.data);
+        
+        fs.writeFileSync(`img/${i}.jpg`, imgResult.data);
+      }
+
       const detailUrl = $(el).find('a.news_tit').prop('href');
       
       async function getDetail() {
-        // const agent = new https.Agent({
-        //   rejectUnauthorized: false, // Only set this to true in a development environment
-        //   secureProtocol: 'TLSv1_method', // Specify the TLS version (e.g., 'TLSv1_2')
-        // });
 
         const detail_url = `${detailUrl}`
 
         try {
-          const res = await axios.get(detail_url);
-          const $ = cheerio.load(res.data);
-          const article_detail = $.html();
+          const res = await axios.get(detail_url, {
+            // RestError: write EPROTO 40736C4DF87F0000:error:0A000152:SSL routines:final_renegotiate:unsafe legacy renegotiation disabled:../deps/openssl/openssl/ssl/statem/extensions.c:922: 에러가 발생했을 때 해결하는 방법
+            httpsAgent: new https.Agent({
+              secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT,
+            })
+          });
+
+          const article_detail = res.data;
 
           return article_detail;
         } catch (err) {
@@ -42,16 +66,16 @@ async function getData() {
       const article_detail = await getDetail();
 
       data[i] = {
-        '제목': title,
-        '신문사': press,
-        '요약 설명': summary,
-        '이미지': imgData,
-        '본문': article_detail,
+        'title': title,
+        'press': press,
+        'summary': summary,
+        'image_url': imgData,
+        'details': article_detail,
       }
     }));
 
     fs.writeFileSync('./news.json', JSON.stringify(data, null, 2));
-    // console.log($('a.news_tit').text());
+    
   } catch (err) {
     // console.log(err);
   }
